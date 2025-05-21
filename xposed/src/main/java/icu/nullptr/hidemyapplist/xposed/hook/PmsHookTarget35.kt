02c24184822b660aa35d90ce5339101ccca15718
Teemo <58159901+liqideqq@users.noreply.github.com>
2025-05-21 18:11:30 +0800

    Create PmsHookTarget35.kt

---
+++ xposed/src/main/java/icu/nullptr/hidemyapplist/xposed/hook/PmsHookTarget35.kt
@@ -0,0 +1,95 @@
+package icu.nullptr.hidemyapplist.xposed.hook
+
+import android.os.Binder
+import android.os.Build
+import androidx.annotation.RequiresApi
+import com.github.kyuubiran.ezxhelper.utils.findMethod
+import com.github.kyuubiran.ezxhelper.utils.findMethodOrNull
+import com.github.kyuubiran.ezxhelper.utils.hookBefore
+import de.robv.android.xposed.XC_MethodHook
+import icu.nullptr.hidemyapplist.common.Constants
+import icu.nullptr.hidemyapplist.xposed.*
+import java.util.concurrent.atomic.AtomicReference
+
+@RequiresApi(Build.VERSION_CODES.PUMPKIN_CAKE)
+class PmsHookTarget35(private val service: HMAService) : IFrameworkHook {
+
+    companion object {
+        private const val TAG = "PmsHookTarget35"
+    }
+
+    private val getPackagesForUidMethod by lazy {
+        findMethod("com.android.server.pm.Computer") {
+            name == "getPackagesForUid"
+        }
+    }
+
+    private var hook: XC_MethodHook.Unhook? = null
+    private var exphook: XC_MethodHook.Unhook? = null
+    private var lastFilteredApp: AtomicReference<String?> = AtomicReference(null)
+
+    @Suppress("UNCHECKED_CAST")
+    override fun load() {
+        logI(TAG, "Load hook")
+        hook = findMethod("com.android.server.pm.AppsFilterImpl", findSuper = true) {
+            name == "shouldFilterApplication"
+        }.hookBefore { param ->
+            runCatching {
+                val snapshot = param.args[0]
+                val callingUid = param.args[1] as Int
+                if (callingUid == Constants.UID_SYSTEM) return@hookBefore
+                val callingApps = Utils.binderLocalScope {
+                    getPackagesForUidMethod.invoke(snapshot, callingUid) as Array<String>?
+                } ?: return@hookBefore
+                val targetApp = Utils.getPackageNameFromPackageSettings(param.args[3]) // PackageSettings <- PackageStateInternal
+                for (caller in callingApps) {
+                    if (service.shouldHide(caller, targetApp)) {
+                        param.result = true
+                        service.filterCount++
+                        val last = lastFilteredApp.getAndSet(caller)
+                        if (last != caller) logI(TAG, "@shouldFilterApplication: query from $caller")
+                        logD(TAG, "@shouldFilterApplication caller: $callingUid $caller, target: $targetApp")
+                        return@hookBefore
+                    }
+                }
+            }.onFailure {
+                logE(TAG, "Fatal error occurred, disable hooks", it)
+                unload()
+            }
+        }
+
+        // Exploit hook for AOSP changes
+        exphook = findMethodOrNull("com.android.server.pm.PackageManagerService", findSuper = true) {
+            name == "getArchivedPackageInternal"
+        }?.hookBefore { param ->
+            runCatching {
+                val callingUid = Binder.getCallingUid()
+                if (callingUid == Constants.UID_SYSTEM) return@hookBefore
+                val callingApps = Utils.binderLocalScope {
+                    service.pms.getPackagesForUid(callingUid)
+                } ?: return@hookBefore
+                val targetApp = param.args[0].toString()
+                for (caller in callingApps) {
+                    if (service.shouldHide(caller, targetApp)) {
+                        param.result = null
+                        service.filterCount++
+                        val last = lastFilteredApp.getAndSet(caller)
+                        if (last != caller) logI(TAG, "@getArchivedPackageInternal: query from $caller")
+                        logD(TAG, "@getArchivedPackageInternal caller: $callingUid $caller, target: $targetApp")
+                        return@hookBefore
+                    }
+                }
+            }.onFailure {
+                logE(TAG, "Fatal error occurred, disable hooks", it)
+                unload()
+            }
+        }
+    }
+
+    override fun unload() {
+        hook?.unhook()
+        hook = null
+        exphook?.unhook()
+        exphook = null
+    }
+}
