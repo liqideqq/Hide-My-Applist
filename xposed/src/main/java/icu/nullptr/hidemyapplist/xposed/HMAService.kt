4288e946aef85aea833300fc2eabb6493f6447dd
Teemo <58159901+liqideqq@users.noreply.github.com>
2025-05-21 18:25:16 +0800

    Update HMAService.kt

--- xposed/src/main/java/icu/nullptr/hidemyapplist/xposed/HMAService.kt
+++ xposed/src/main/java/icu/nullptr/hidemyapplist/xposed/HMAService.kt
@@ -118,9 +118,7 @@ class HMAService(val pms: IPackageManager) : IHMAService.Stub() {
             if (it.flags and ApplicationInfo.FLAG_SYSTEM != 0) it.packageName else null
         }
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.PUMPKIN_CAKE) {
-            frameworkHooks.add(PmsHookTarget35(this))
-        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
             frameworkHooks.add(PmsHookTarget34(this))
         } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             frameworkHooks.add(PmsHookTarget33(this))
